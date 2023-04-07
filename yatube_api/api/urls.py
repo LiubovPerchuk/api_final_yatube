e340c9bb4d3e5e282858587e7b5f6756cc1d49ac
Любовь Перчук <lyuba_box@mail.ru>
2023-04-07 22:47:07 +0300

    Второе ревью

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -5,16 +5,16 @@ from .views import (
     CommentViewSet, GroupViewSet, FollowViewSet, PostViewSet)
 
 
-router = routers.DefaultRouter()
-router.register("posts", PostViewSet)
-router.register("groups", GroupViewSet)
-router.register(
+router_v1 = routers.DefaultRouter()
+router_v1.register("posts", PostViewSet)
+router_v1.register("groups", GroupViewSet)
+router_v1.register(
     r"posts/(?P<post_id>\d+)/comments", CommentViewSet, basename="comments")
-router.register("follow", FollowViewSet, basename="follow")
+router_v1.register("follow", FollowViewSet, basename="follow")
 
 
 urlpatterns = [
-    path("v1/", include(router.urls)),
+    path("v1/", include(router_v1.urls)),
     path("v1/", include("djoser.urls")),
     path("v1/", include("djoser.urls.jwt")),
 ]
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -17,8 +17,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
     "djoser",
+
     "api",
     "posts",
 ]
